7b20396c8c427ba0b658e0b9cc6b26ccf657ed0b
grig9 <70834200+grig9@users.noreply.github.com>
2022-03-02 16:13:17 +0300

    bootstrap image

--- example-app/resources/views/main.blade.php
+++ example-app/resources/views/main.blade.php
@@ -1,5 +1,38 @@
 @extends('layout')
 
 @section('content')
-<h1>Main page</h1>
+
+<div class="container">
+  <h1 class="text-center mt-3">Main page</h1>
+  <div class="row mt-5">
+    <div class="col-3">
+      <div>
+        <img src="../image.png" alt="asdfasd" width="300">
+      </div>
+      <div class="d-grid gap-3 mt-3">
+        <button type="button" class="btn btn-info">Info</button>
+        <button type="button" class="btn btn-warning">Warning</button>
+        <button type="button" class="btn btn-danger">Danger</button>
+      </div>
+    </div>
+    <div class="col-3">
+      <div>
+        <img src="../image.png" alt="asdfasd" width="300">
+      </div>
+    </div>
+    <div class="col-3">
+      <div>
+        <img src="../image.png" alt="asdfasd" width="300">
+      </div>
+    </div>
+    <div class="col-3">
+      <div>
+        <img src="../image.png" alt="asdfasd" width="300">
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
 @endsection
