9a2e1c5bef39c5ecfab5a46fdfa6f851723d3b2d
grig9 <70834200+grig9@users.noreply.github.com>
2022-03-10 16:09:33 +0300

    factory

---
+++ example-app/database/factories/PostFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    protected $model = Post::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->word,
+            'slug' => $this->faker->slug,
+            'content' => $this->faker->text,
+            'date' => $this->faker->date,
+            'user_id' => User::factory()->create() // User::factory()->create() возвращает id
+        ];
+    }
+}
